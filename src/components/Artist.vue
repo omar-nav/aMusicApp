1a53257b7ae2592503ae3a4bbff0ebdc72354e29
omar-nav <omar.navarro@g.austincc.edu>
2019-03-08 08:53:30 -0600

    allow user to change top artist by country

--- src/components/Artist.vue
+++ src/components/Artist.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
   li.artist
     h2: a(:href="artist.url" target="_blank") {{ artist.name }}
-    img(:src="artist.image[3]['#text']")
+    img(:src="artist.image[2]['#text']")
 </template>
 
 <script>
